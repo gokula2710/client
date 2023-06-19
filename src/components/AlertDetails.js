e8728e53ec3627ff4975fd5cbcbd09e62695d2b3
gokula2710 <135031362+gokula2710@users.noreply.github.com>
2023-06-19 13:00:42 +0530

    Add files via upload

--- src/components/AlertDetails.js
+++ src/components/AlertDetails.js
@@ -1,16 +1,17 @@
+import logo from '../logo.png'
+import { format } from 'date-fns'
 const AlertDetails = ({alerts}) => {
     return (
-        <div className="equipment-view">
-                    <div className="image">
-                        <img src={''} alt="equipment" /></div>
+        <div className="computer-logo">
+                    <div className="lab-computer">
+                        <img src={logo} alt="equipment" /></div>
                     <div className="info">
                         <h2>Name: <span className="value">{alerts.name}</span></h2>
                         <h2>Type: <span className="value">{alerts.type}</span></h2>
                         <h2>Brand: <span className="value">{alerts.brand}</span></h2>
-                        <h2>Date of Purchase: <span className="value">{alerts.dop}</span></h2>
-                        <h2>Warranty: <span className="value">{alerts.warranty}</span></h2>
+                        <h2>Date of Purchase: <span className="value">{format(new Date(alerts.dop), 'dd-MM-yyyy')}</span></h2>
+                        <h2>Warranty: <span className="value">{format(new Date(alerts.warranty),'dd-MM-yyyy')}</span></h2>
                         <h2>Condition: <span className="value">{alerts.condition}</span></h2>
-                        <h2>Location: <span className="value">{alerts.location}</span></h2>
                         <h2>Lab: <span className="value">{alerts.lab}</span></h2>
         
                     </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,21 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useLocation } from "react-router-dom";
 import EquipmentDetails from "../components/EquipmentDetails";
 import EquipmentForm from "../components/EquipmentForm";
 import { useEquipmentsContext } from "../hooks/useEquipmentContext";
 import { useAuthContext } from '../hooks/useAuthContext'
 const Home = () => {
-    const eqpCopy=useLocation().state?.eqp
+    const eqpCopy = useLocation().state?.eqp
     const { workouts, dispatch } = useEquipmentsContext()
     const { user } = useAuthContext()
+    const [search, setSearch] = useState(workouts)
     useEffect(() => {
-        
+
         const getWorkouts = async () => {
             try {
                 const response = await fetch(`${process.env.REACT_APP_SERVER_URI}/api/equipments`, {
-                    headers:{
-                        'Authorization':`Bearer ${user.token}`
+                    headers: {
+                        'Authorization': `Bearer ${user.token}`
                     }
                 })
                 const json = await response.json()
@@ -34,43 +35,70 @@ const Home = () => {
         if (user) {
             getWorkouts()
         }
-        else{
+        else {
             console.log("Authorization required")
         }
 
 
-    }, [dispatch,user])
+    }, [dispatch, user])
+    const handleSearch = async (e) => {
+        e.preventDefault()
+       try{
+        const response = await fetch(`${process.env.REACT_APP_SERVER_URI}/api/equipments/search`, {
+            headers: {
+                'Authorization': `Bearer ${user.token}`,
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ name: e.target.value })
+        })
+        const json = await response.json()
+        if (response.ok) {
+            console.log(json)
+            setSearch(json)
+        }
+        else {
+            console.log(json)
+        }
+       }
+       catch(error){
+              console.log(error.message)
+         }
 
-   if(!eqpCopy){
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout, index) => (
-                    <div key={index}>
-                        <EquipmentDetails workout={workout} key={workout._id} />
-                    </div>
-                ))
-                }
-            </div>
-            <EquipmentForm />
-        </div>
-    )
-}
-    else{
-        return(
+    }
+    if (!eqpCopy) {
+        return (
             <div className="home">
-            <div className="workouts">
-                {eqpCopy && eqpCopy.map((eqpCopy, index) => (
-                    <div key={index}>
-                        <EquipmentDetails workout={eqpCopy} key={eqpCopy._id}  />
-                    </div>
-                ))
-                }
+                <div className="workouts">
+                    <form className="search">
+                        <input onChange={handleSearch} type="text" placeholder="Search" />
+                    </form>
+                    {search && search.map((workout, index) => (
+                        <div key={index}>
+                            <EquipmentDetails workout={workout} key={workout._id} />
+                        </div>
+                    ))
+                    }
+                </div>
+                <EquipmentForm />
             </div>
-            <EquipmentForm />
-        </div>
         )
     }
-   }
+    else {
+        return (
+            <div className="home">
+                <div className="workouts">
+                    {eqpCopy && eqpCopy.map((eqpCopy, index) => (
+                        <div key={index}>
+                            <EquipmentDetails workout={eqpCopy} key={eqpCopy._id} />
+                        </div>
+                    ))
+                    }
+                </div>
+                <EquipmentForm />
+            </div>
+        )
+    }
+}
 
 export default Home;
