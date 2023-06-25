419e3d0aadc2ca9643189fdcfb51970953b87b5f
gokula2710 <135031362+gokula2710@users.noreply.github.com>
2023-06-25 14:52:28 +0530

    Update AlertDetails.js

--- src/components/AlertDetails.js
+++ src/components/AlertDetails.js
@@ -5,9 +5,8 @@ const AlertDetails = ({alerts}) => {
         <div className="computer-logo">
                     <div className="alert-info">
                         <h2>Name: <span className="value">{alerts.name}</span></h2>
-                        <h2>Type: <span className="value">{alerts.type}</span></h2>
+                        <p>Warranty Expiring for {alerts.name} on {format(new Date(alerts.warranty),'dd-MM-yyyy')}</p>
                         <h2>Date of Purchase: <span className="value">{format(new Date(alerts.dop), 'dd-MM-yyyy')}</span></h2>
-                        <h2>Warranty: <span className="value">{format(new Date(alerts.warranty),'dd-MM-yyyy')}</span></h2>
                         <h2>Lab: <span className="value">{alerts.lab}</span></h2>
         
                     </div>
