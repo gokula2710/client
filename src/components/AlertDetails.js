2c133cbea2a6ff02b2f0c83c07e0c4c7e845f1ef
gokula2710 <135031362+gokula2710@users.noreply.github.com>
2023-06-25 15:04:46 +0530

    Update AlertDetails.js

--- src/components/AlertDetails.js
+++ src/components/AlertDetails.js
@@ -6,7 +6,7 @@ const AlertDetails = ({alerts}) => {
                     <div className="alert-info">
                         <h2><span className="value">{alerts.name}</span></h2>
                         <p>Warranty Expiring for {alerts.name} on {format(new Date(alerts.warranty),'dd-MM-yyyy')}</p>
-                        <p>Lab:{alerts.lab}</p>
+                        <p>Lab: {alerts.lab}</p>
                     </div>
                 </div>
       );
